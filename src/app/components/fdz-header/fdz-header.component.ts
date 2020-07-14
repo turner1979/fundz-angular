4100101e6950cdb8706c113cf8e55e0dd20f374d
Scott Turner <sturner1979@hotmail.com>
2020-07-14 13:31:42 +0100

    added back button to header

--- src/app/components/fdz-header/fdz-header.component.ts
+++ src/app/components/fdz-header/fdz-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'fdz-header',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FdzHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+
+  }
+
+  onBackClick() {
+    this.router.url === '/funds' ? this.router.navigate(['/']) : this.router.navigate(['/funds']);
   }
 
 }
