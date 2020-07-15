036bc5b317d122ee1486b020a42146bd05cb7d5d
Scott Turner <sturner1979@hotmail.com>
2020-07-15 09:30:57 +0100

    update for logo

--- src/app/components/fdz-logo/fdz-logo.component.ts
+++ src/app/components/fdz-logo/fdz-logo.component.ts
@@ -7,20 +7,12 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class FdzLogoComponent implements OnInit {
 
-  @Input() colour = '#222222';
-  @Input() fontSize = '16px';
+  @Input() isHome = false;
 
   constructor() { }
 
   ngOnInit(): void {
 
-  }
-
-  get logoStyles() {
-    return {
-      'color' : this.colour,
-      'font-size' : this.fontSize
-    }
   }
 
 }
